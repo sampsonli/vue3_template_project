62814de8b1d06ef5f6f4395729e2748576b13209
李春 <lichun5@sfmail.sf-express.com>
2023-10-10 15:11:11 +0800

    upgrade

--- src/views/Home/HomeModel.js
+++ src/views/Home/HomeModel.js
@@ -1,15 +1,9 @@
 import {define, Model} from "mtor-vue";
-
 @define(module)
 class HomeModel extends Model {
     num = 1;
-    async init() {
-        this.num = 277;
-    }
     addNum() {
-        this.num+=20;
+        this.num+=2;
     }
-
-
 }
 export default HomeModel;
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -2,16 +2,16 @@
   <div class="home-page">
     num={{model.num}}
     <br/>
-    <div class="btn" @click="model.addNum">测试1</div>
+    <div class="btn" @click="model.addNum">测试+1</div>
 
   </div>
 
 </template>
 
 <script setup>
-import {useInitModel} from "mtor-vue";
+import {useModel} from "mtor-vue";
 import HomeModel from "~/views/Home/HomeModel";
-const model = useInitModel(HomeModel, ({init})=> init());
+const model = useModel(HomeModel);
 </script>
 
 <style scoped lang="less" src="./style.less"></style>
